a0228948d87824e7a91a45521d70a39931ab3b46
Giulia Martins <108757011+Giulia-Martins@users.noreply.github.com>
2023-07-10 22:34:39 -0300

    Add files via upload

---
+++ scrip python youtube class.py
@@ -0,0 +1,19 @@
+name = input ("What is your name?") 
+age = input ("How old are you?")
+weight = input ("What is your weight?")
+
+print("Hello", name, "welcome to the app")
+
+day = input ("What is your birth day?")
+month = input ("What is your month of birth?")
+year = input ("What is your year of birth?")
+
+print("Your Birth is", month, day, year,",right?")
+
+
+
+
+
+
+
+
